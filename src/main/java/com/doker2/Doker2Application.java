5cc1e40c5e613dab475b337ab46baf414cb73f67
enable01 <!jeong1016>
2019-03-27 02:33:00 +0900

    init

---
+++ src/main/java/com/doker2/Doker2Application.java
@@ -0,0 +1,13 @@
+package com.doker2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Doker2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Doker2Application.class, args);
+    }
+
+}
